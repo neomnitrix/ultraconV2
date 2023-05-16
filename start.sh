b9a03dbbe905960f6fe8780cf44077cf6be73607
neomnitrix <127597790+neomnitrix@users.noreply.github.com>
2023-05-16 13:27:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/neomnitrix/ultraconV2.git /ultraconV2
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /ultraconV2
 fi
-cd /URL-Shortener-V2
+cd /ultraconV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
