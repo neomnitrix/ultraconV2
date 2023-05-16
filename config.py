3b2ed4e8f588e23bcc31ce9daf5217f0fb698cdc
neomnitrix <127597790+neomnitrix@users.noreply.github.com>
2023-05-16 11:34:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,26 +16,26 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID", "23550047"))
+API_ID = int(os.environ.get("API_ID"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH", "34dbf50d81fb975b4813e99f5aa1101f")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6075544608:AAFuMszvgpWC4cfpJy2-LXque6UwxyhSnSU")  # Bot token from @BotFather
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Bot token from @BotFather
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS").split("5891346529,5439467749")]
+    [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
     if os.environ.get("ADMINS")
     else []
 )
 
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
 DATABASE_URL = os.environ.get(
-    "mongodb+srv://carryminati:carryminati@cluster0.rf3byeq.mongodb.net/?retryWrites=true&w=majority", None
+    "DATABASE_URL", None
 )  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID", "5891346529"))  # id of the owner
+OWNER_ID = int(os.environ.get("OWNER_ID"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
 LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "-1001950298408")
+    os.environ.get("LOG_CHANNEL")
 )  # log channel for information about users
 UPDATE_CHANNEL = os.environ.get(
     "UPDATE_CHANNEL", False)  # For Force Subscription
@@ -60,7 +60,7 @@ BASE_SITE = os.environ.get("BASE_SITE", "droplink.co")
 CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
 CHANNEL_ID = (
     [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
-    if os.environ.get("CHANNEL_ID", "-1001950298408")
+    if os.environ.get("CHANNEL_ID")
     else []
 )
 
