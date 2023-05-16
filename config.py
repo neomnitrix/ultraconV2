bf893523fb1a7f501c6df61d77e1bdae2de968b4
neomnitrix <127597790+neomnitrix@users.noreply.github.com>
2023-05-16 11:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,26 +16,26 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", "23550047"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Bot token from @BotFather
+API_HASH = os.environ.get("API_HASH", "34dbf50d81fb975b4813e99f5aa1101f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6075544608:AAFuMszvgpWC4cfpJy2-LXque6UwxyhSnSU")  # Bot token from @BotFather
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
+    [int(i.strip()) for i in os.environ.get("ADMINS").split("5891346529,5439467749")]
     if os.environ.get("ADMINS")
     else []
 )
 
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", None
+    "mongodb+srv://carryminati:carryminati@cluster0.rf3byeq.mongodb.net/?retryWrites=true&w=majority", None
 )  # mongodb uri from https://www.mongodb.com/
 OWNER_ID = int(os.environ.get("OWNER_ID"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
 LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "0")
+    os.environ.get("LOG_CHANNEL", "-1001950298408")
 )  # log channel for information about users
 UPDATE_CHANNEL = os.environ.get(
     "UPDATE_CHANNEL", False)  # For Force Subscription
@@ -46,7 +46,7 @@ IS_PRIVATE = is_enabled(
     os.environ.get("IS_PRIVATE", "False"), "False"
 )  # true for private use and restricting users
 SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2"
+    "SOURCE_CODE", "https://github.com/neomnitrix/ultraconV2"
 )  # for upstream repo
 # image when someone hit /start
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
@@ -60,7 +60,7 @@ BASE_SITE = os.environ.get("BASE_SITE", "droplink.co")
 CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
 CHANNEL_ID = (
     [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
-    if os.environ.get("CHANNEL_ID")
+    if os.environ.get("CHANNEL_ID", "-1001950298408")
     else []
 )
 
